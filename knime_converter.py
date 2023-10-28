ea0a0fb28c9af1ebb52f05ac8a64e2b110d2490d
YS0meone <timyuan1016@gmail.com>
2023-10-28 16:06:44 -0700

    Refactored the parser so that we can add operator mapping more easily and support better xml parsing

--- knime_converter.py
+++ knime_converter.py
@@ -5,118 +5,124 @@ from knime_operator_generator import OperatorGenerator
 import re
 import uuid
 
-XML_FILE_PATH = "workflow.xml"
+# relative path to the files
+XML_FILE_PATH = "workflow2.xml"
 JSON_FILE_PATH = "output.json"
-CURRENT_OUTPUT = "progress.json"
 
+# texera workflow template
 workflow_temp = {
-            "operators": [],
-            "operatorPositions": {},
-            "links": [],
-            "groups": [],
-            "breakpoints": {},
-            "commentBoxes": []
-        }
-
+    "operators": [],
+    "operatorPositions": {},
+    "links": [],
+    "groups": [],
+    "breakpoints": {},
+    "commentBoxes": []
+}
 
 
 def read_xml(path):
+    """
+        read the knime workflow xml file and convert to the dictionary
+    """
     with open(path, 'r') as xml_file:
-        data_dict = xmltodict.parse(xml_file.read(), dict_constructor=dict)
-        return(data_dict['config']['config'])
-    
+        data_dict = xmltodict.parse(
+            xml_file.read(), dict_constructor=dict)
+        return data_dict
+
 
 def get_output(path, data):
+    """
+        dump the dictionary as json file for texera to read
+    """
     with open(path, 'w') as json_file:
         json.dump(data, json_file, indent=4)
-    
+
+
 def remove_pattern(input_str):
-    pattern  = r' \(\#\d+\)/settings.xml'
+    """
+        remove the pattern to get the knime's operator type
+    """
+    pattern = r' \(\#\d+\)/settings.xml'
     return re.sub(pattern, '', input_str)
 
-def generate_link(mapping, connection):
 
+def generate_link(mapping, connection):
+    """
+        Given the operatorID mappings between knime and texera, and the connection section of knime, create connection element in the texera workflow to generate links
+    """
     template = {
-            "linkID": "link-"+str(uuid.uuid4()),
-            "source": {
-                "operatorID": "",
-                "portID": ""
-            },
-            "target": {
-                "operatorID": "",
-                "portID": ""
-            }
+        "linkID": "link-"+str(uuid.uuid4()),
+        "source": {
+            "operatorID": "",
+            "portID": ""
+        },
+        "target": {
+            "operatorID": "",
+            "portID": ""
         }
-    # template[]
-    sourceID = connection['entry'][0]['@value']
-    sourcePort = int(connection['entry'][2]['@value'])
-    destID = connection['entry'][1]['@value']
-    destPort = int(connection['entry'][3]['@value'])
+    }
+    sourceID = connection["sourceID"]
+    sourcePort = int(connection["sourcePort"])
+    destID = connection["destID"]
+    destPort = int(connection["destPort"])
     template["source"]["operatorID"] = mapping[sourceID]
     template["source"]["portID"] = "output-" + str(sourcePort - 1)
     template["target"]["operatorID"] = mapping[destID]
     template["target"]["portID"] = "input-" + str(destPort - 1)
-    
+
     return template
 
-def main():
-    # parse xml
-    kn_dict = read_xml(XML_FILE_PATH)
-    tx_dict = workflow_temp.copy()
-    k2t_mapping = {}
-    # pprint(kn_dict)
-    kn_ops = kn_dict[2]["config"]
-    # pprint(kn_ops)
-    # parsing the operator part
-    for op in kn_ops:
-        # pprint(op)
 
-        op_type = remove_pattern(op['entry'][1]['@value'])
+def format_dict(xml_dict, ret_dict):
+    """
+        Further process the parse Knime workflow dictionary to make the keys and values more meaningful
+    """
+    if "entry" in xml_dict:
+        if isinstance(xml_dict["entry"], list):
+            for entry in xml_dict["entry"]:
+                ret_dict[entry["@key"]] = entry["@value"]
+        else:
+            ret_dict[xml_dict["entry"]["@key"]] = xml_dict["entry"]["@value"]
+    if "config" in xml_dict:
+        if isinstance(xml_dict["config"], list):
+            for config in xml_dict["config"]:
+                ret_dict[config["@key"]] = {}
+                format_dict(config, ret_dict[config["@key"]])
+        else:
+            ret_dict[xml_dict["config"]["@key"]] = {}
+            format_dict(xml_dict["config"],
+                        ret_dict[xml_dict["config"]["@key"]])
+    return
+
+
+def main():
+    # preprocess the input workflow
+    xml_dict = read_xml(XML_FILE_PATH)
+    kn_dict = {}
+    format_dict(xml_dict, kn_dict)
+    # the dictionary representation of the knime workflow in dict
+    kn_dict = kn_dict["workflow.knime"]
+    tx_dict = workflow_temp.copy()
+    # the operatorID mapping between knime and texera
+    k2t_mapping = {}
+    kn_ops = kn_dict["nodes"]
+
+    # parsing the operator part
+    for op in kn_ops.values():
+        # get the operator type of the knime operator
+        op_type = remove_pattern(op["node_settings_file"])
+        # initialize an operator generator to create operator elements in texera workflow
         og = OperatorGenerator(op_type, op)
-        # pprint(op)
         tx_dict["operators"].append(og.get_temp())
         tx_dict["operatorPositions"][og.get_id()] = og.generate_pos()
-        k2t_mapping[op["entry"][0]["@value"]] = og.get_id()
+        k2t_mapping[op["id"]] = og.get_id()
+
     # parsing the connection part
-    
-    # print(k2t_mapping)
-    # pprint(kn_dict[-1])
-    # pprint(generate_link())
-    connections = kn_dict[-1]['config']
-    if type(connections) != list:
-        connections = [connections]
-    # pprint(connections)
-    for connection in connections:
+    connections = kn_dict["connections"]
+    for connection in connections.values():
         tx_dict["links"].append(generate_link(k2t_mapping, connection))
 
-    get_output(CURRENT_OUTPUT, tx_dict)
-    
-        # tx_dict["operatorPositions"][og.get_id()] = og.generate_pos()
-
-
-    # tx_dict = workflow_temp.copy()
-    # get_output(JSON_FILE_PATH, rm_dict)
-
-    # # generate show result operator
-
-    # # create the operator dict
-    # for operator in rm_dict["operator"]:
-    #     op_type = mapping_table[operator["@class"]]
-    #     og = OperatorGenerator(op_type, operator)
-    #     tx_dict["operators"].append(og.get_temp())
-    #     tx_dict["operatorPositions"][og.get_id()] = og.generate_pos()
-    # # get operator position
-    # get_output(CURRENT_OUTPUT, tx_dict)
-    # get connects
-
-
-
-    # generate tx_dict
-    # for operator in rm_dict["operator"]:
-    #     print(operator)
-        # if operator["@class"] in mapping_table:
-
-    
+    get_output(JSON_FILE_PATH, tx_dict)
 
 
 if __name__ == "__main__":
