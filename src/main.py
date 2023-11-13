117f20eeaa9c7c6c723f3c0cfedbc029e10a09dc
YS0meone <timyuan1016@gmail.com>
2023-11-12 22:58:29 -0800

    Added system arguments for input output path and set up shell scripts for each examples

--- src/main.py
+++ src/main.py
@@ -1,16 +1,12 @@
 import json
 import xmltodict
 from pprint import pprint
-from knime_operator_generator import OperatorGenerator
+from operator_generator import OperatorGenerator
 import re
 import uuid
 from pathlib import Path
 import os
-
-# relative path to the files
-ROOT_PATH = Path("./examples/basic_csv_read")
-XML_FILE_PATH = ROOT_PATH / "workflow.xml"
-JSON_FILE_PATH = ROOT_PATH / "output.json"
+import argparse
 
 # texera workflow template
 workflow_temp = {
@@ -23,11 +19,26 @@ workflow_temp = {
 }
 
 
+def parse_args():
+    """
+        Parse the system arguments and return them
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input", type=Path, required=True,
+                        help="Path to the directory of the workflow")
+    parser.add_argument("--output", type=Path, required=True,
+                        help="Path to the output folder")
+    parser.add_argument("--config", type=Path, required=True,
+                        help="Path to the mapping config file")
+    args = parser.parse_args()
+    return args
+
+
 def read_xml(path):
     """
         read the knime workflow xml file and convert to the dictionary
     """
-    with open(path, 'r') as xml_file:
+    with open(path, 'r', encoding="utf-8") as xml_file:
         data_dict = xmltodict.parse(
             xml_file.read(), dict_constructor=dict)
         return data_dict
@@ -99,14 +110,20 @@ def format_dict(xml_dict, ret_dict):
 
 
 def main():
+    # get the root path from the system arguments
+    args = parse_args()
+    root_path = args.input
+    xml_path = root_path / "workflow.xml"
+    output_path = args.output
+    config_path = args.config
     # check if we need to convert workflow.knime
-    if not XML_FILE_PATH.exists():
-        knime_path = ROOT_PATH / "workflow.knime"
+    if not xml_path.exists():
+        knime_path = root_path / "workflow.knime"
         if knime_path.exists():
-            knime_path.rename(XML_FILE_PATH)
+            knime_path.rename(xml_path)
 
     # preprocess the input workflow
-    xml_dict = read_xml(XML_FILE_PATH)
+    xml_dict = read_xml(xml_path)
     kn_dict = {}
     format_dict(xml_dict, kn_dict)
     # the dictionary representation of the knime workflow in dict
@@ -121,7 +138,7 @@ def main():
         # get the operator type of the knime operator
         op_type = remove_pattern(op["node_settings_file"])
         # initialize an operator generator to create operator elements in texera workflow
-        og = OperatorGenerator(op_type, op, ROOT_PATH)
+        og = OperatorGenerator(op_type, op, root_path, config_path)
         tx_dict["operators"].append(og.get_temp())
         tx_dict["operatorPositions"][og.get_id()] = og.generate_pos()
         k2t_mapping[op["id"]] = og.get_id()
@@ -131,7 +148,7 @@ def main():
     for connection in connections.values():
         tx_dict["links"].append(generate_link(k2t_mapping, connection))
 
-    get_output(JSON_FILE_PATH, tx_dict)
+    get_output(output_path, tx_dict)
 
 
 if __name__ == "__main__":
